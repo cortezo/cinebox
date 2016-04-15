214b9e3ca149e8f769e0a2820f9bdb422e014859
Victor W <vwalldev@gmail.com>
2016-04-14 21:05:48 -0700

    Added forms for Add Movie, Add Genre.  Tweaked view for movies index.

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -4,14 +4,28 @@ class GenresController < ApplicationController
   end
 
   def new
-
+    @genre = Genre.new
   end
 
   def create
+    @genre = Genre.new(genre_params)
 
+    if @genre.save
+      flash[:success] = "Genre created successfully."
+      redirect_to root_path
+    else
+      flash[:error] = "Something went wrong."
+      render :new
+    end
   end
 
   def show
 
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
   end
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,15 +4,34 @@ class MoviesController < ApplicationController
     @movies = Movie.all
   end
 
-  def show
+  def new
+    @movie = Movie.new
+  end
 
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      flash[:success] = "Movie successfully created."
+      redirect_to root_path
+    else
+      flash[:error] = "Something went wrong."
+      render :new
+    end
+  end
+
+  def show
   end
 
   def edit
-
   end
 
   def update
+  end
 
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :file_loc, :genre_id)
   end
 end
